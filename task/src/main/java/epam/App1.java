01eb52b68398f8eaa5d9821c6c1880c323035b6e
Mihail Suhonosov <sms069@bk.ru>
2020-10-13 23:56:36 +0300

    task1 done

--- task/src/main/java/epam/App1.java
+++ task/src/main/java/epam/App1.java
@@ -1,13 +1,18 @@
 package epam;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * 1. Приветствовать любого пользователя при вводе его имени через командную строку.
  *
  */
 public class App1
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String name;
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        System.out.println("Приветствую тебя. " + name + "!!!");
     }
 }
