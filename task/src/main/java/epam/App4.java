bd7c2a15382cc47977f48581d4061ba488b6a1c1
Mihail Suhonosov <sms069@bk.ru>
2020-11-15 01:08:47 +0300

    App4 ver 2

--- task/src/main/java/epam/App4.java
+++ task/src/main/java/epam/App4.java
@@ -5,58 +5,39 @@ package epam;
  *    вывести результат на консоль.
  */
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App4 {
     public static void main(String[] args) {
-        // Если метод запущен без аргументов, то предлагается ввести эти аргументы
-        if (isArrayEmpty(args)) {
-            // Обработка аргументов метода
-            System.out.println("Длинна массива " + args.length + " Хэш код " + args[0].hashCode());
-            System.out.println("Метод запущен с аргументами" + Arrays.deepToString(args));
-            System.out.print("Из них числами являются: ");
-            int [] masInt = new int[args.length]; // Массив чисел (аргументов)
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].matches("^-?[0-9]+")) { // Если аргумент содержит только целое число
-                    try {
-                        masInt[i] = Integer.parseInt(args[i]); // преобразование строки в число
-                        System.out.print(masInt[i] + " ");
-                    } catch (NumberFormatException e) {
-                        System.out.println("Введено слишком большое число");
-                        args = new String[0];
-                        System.out.println(Arrays.deepToString(args));;
-                        main(args);
-                    }
-                }
-            }
-
+        int [] masInt = new int[args.length]; // Массив чисел (аргументов)
+        // Если все числа валидны
+        if (checkArrayToInt(args, masInt)) {
             int sum = 0; // сумма чисел
             int pr = 1; // произведение чисел
-
-            // цикл для подсчета суммы и произведения чисел (аргументов)
             boolean flagSum = true;
             boolean flagPr = true;
+            // цикл для подсчета суммы и произведения чисел (аргументов)
             for (int k: masInt) {
                 if (checkSum(sum, k)) {
                     sum += k;
-                } else  {
-                    System.out.println("\nПревышено максимальное или минимальное значение для вычисления суммы и произведения чисел");
+                } else {
+                    System.out.println("Превышено максимальное или минимальное значение для вычисления суммы и произведения чисел");
                     flagSum = flagPr = false;
                     break;
                 }
                 if (checkPr(pr, k)) {
                     pr *= k;
-                } else  {
-                    System.out.println("\nПревышено максимальное или минимальное значение для вычисления произведения чисел");
+                } else {
+                    System.out.println("Превышено максимальное или минимальное значение для вычисления произведения чисел");
                     flagPr = false;
                     break;
                 }
             }
-            System.out.println(flagSum ? ("\nСумма этих чисел = " + sum) : ("Сумму чисел не вычислить"));
-            System.out.println(flagPr ? ("Произведение всех чисел = " + pr) : ("Произведение чисел не вычислить"));
-            System.exit(0); // Если я этого не напишу, то программа завершается не адекватно
+            System.out.println(flagSum ? ("Сумма чисел = " + sum) : ("Сумму чисел не вычислить"));
+            System.out.println(flagPr ? ("Произведение чисел = " + pr) : ("Произведение чисел не вычислить"));
+
         } else {
+            // Если метод запущен без аргументов, то предлагается ввести эти аргументы
             System.out.println("Введите целые числа через пробел и нажмите Enter");
             Scanner scanner = new Scanner(System.in);
             String inText = scanner.nextLine();
@@ -65,31 +46,42 @@ public class App4 {
         }
     }
 
+    // Проверка на пределы значения int при сложении 2-х чисел
     public static boolean checkSum (int valueOne, int valueTwo) {
         long s = (long) valueOne + (long) valueTwo;
-        if (s < Integer.MIN_VALUE || s > Integer.MAX_VALUE) {
-            return false;
-        } else return true;
+        return  (s > Integer.MIN_VALUE && s < Integer.MAX_VALUE);
     }
 
+        // Проверка на пределы значения int при умножении 2-х чисел
     public static boolean checkPr (int valueOne, int valueTwo) {
         long s = (long) valueOne * (long) valueTwo;
-        if (s < Integer.MIN_VALUE || s > Integer.MAX_VALUE) {
-            return false;
-        } else return true;
+        return  (s > Integer.MIN_VALUE && s < Integer.MAX_VALUE);
     }
+
         // Проверка на пустой массив
     public static boolean isArrayEmpty (String [] array) {
-        if (array == null || array.length == 0) {
-            return false;
-        } else {
+        boolean flag;
+        if (flag = (array != null && array.length != 0)) {
             int hash = 0;
             for (String s: array) {
                 hash += s.hashCode();
             }
-            if (hash == 0) {
-                return false;
-            } else  return true;
-        }
+            flag = (hash != 0);
+        } return flag;
     }
+
+    // Проверка массива на валидность данных (целые числа типа int)
+    public static boolean checkArrayToInt (String [] arrayStr, int [] arrayInt) {
+        boolean flag;
+        if (flag = isArrayEmpty(arrayStr)) {
+            for (int i = 0; i < arrayStr.length; i++) {
+                if (flag = arrayStr[i].matches("^-?[0-9]{1,10}")) {
+                    if ( flag = (Long.parseLong(arrayStr[i]) < Integer.MAX_VALUE && Long.parseLong(arrayStr[i]) > Integer.MIN_VALUE) ) {
+                        arrayInt[i] = Integer.parseInt(arrayStr[i]);
+                        }
+                }
+            }
+        } return flag;
+    }
+
 }
