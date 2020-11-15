5960568935289dfe476369f3b8966a90387130e5
Mihail Suhonosov <sms069@bk.ru>
2020-11-16 00:25:01 +0300

    App 5 change & create App5_1 with enum

--- task/src/main/java/epam/App5.java
+++ task/src/main/java/epam/App5.java
@@ -10,57 +10,55 @@ import java.util.Scanner;
 
 public class App5 {
     public static void main(String[] args) {
-        int monthNumber;
         System.out.println("Введите число от 1 до 12");
         Scanner scanner = new Scanner(System.in);
         String month = scanner.nextLine();
-        if (isNumberEmpty(month)) {
-            monthNumber = Integer.parseInt(month);
-            switch (monthNumber) {
-                case  1:
-                    System.out.println("Январь");
-                    break;
-                case  2:
-                    System.out.println("Февраль");
-                    break;
-                case  3:
-                    System.out.println("Март");
-                    break;
-                case  4:
-                    System.out.println("Апрель");
-                    break;
-                case  5:
-                    System.out.println("Май");
-                    break;
-                case  6:
-                    System.out.println("Июнь");
-                    break;
-                case  7:
-                    System.out.println("Июль");
-                    break;
-                case  8:
-                    System.out.println("Август");
-                    break;
-                case  9:
-                    System.out.println("Сентябрь");
-                    break;
-                case  10:
-                    System.out.println("Октябрь");
-                    break;
-                case  11:
-                    System.out.println("Ноябрь");
-                    break;
-                case  12:
-                    System.out.println("Декабрь");
-                    break;
-            }
-        } else {
+        try {
+            int monthNumber = Integer.parseInt(month);
+            if (monthNumber > 0 && monthNumber <= 12) {
+                switch (monthNumber) {
+                    case 1:
+                        System.out.println("Январь");
+                        break;
+                    case 2:
+                        System.out.println("Февраль");
+                        break;
+                    case 3:
+                        System.out.println("Март");
+                        break;
+                    case 4:
+                        System.out.println("Апрель");
+                        break;
+                    case 5:
+                        System.out.println("Май");
+                        break;
+                    case 6:
+                        System.out.println("Июнь");
+                        break;
+                    case 7:
+                        System.out.println("Июль");
+                        break;
+                    case 8:
+                        System.out.println("Август");
+                        break;
+                    case 9:
+                        System.out.println("Сентябрь");
+                        break;
+                    case 10:
+                        System.out.println("Октябрь");
+                        break;
+                    case 11:
+                        System.out.println("Ноябрь");
+                        break;
+                    case 12:
+                        System.out.println("Декабрь");
+                        break;
+                }
+            } else throw new NumberFormatException();
+        } catch (NumberFormatException e) {
             System.out.println("Не допустимое значение!");
             main(args);
         }
-    }
-    public static boolean isNumberEmpty (String s) {
-        return s.matches("[1-9]|1[0-2]");
     }
 }
 
---
+++ task/src/main/java/epam/App5_1.java
@@ -0,0 +1,61 @@
+package epam;
+
+import java.util.Scanner;
+
+/*
+ * 5.   Ввести число от 1 до 12.
+ * Вывести на консоль название месяца, соответствующего данному числу.
+ * Осуществить проверку корректности ввода чисел.
+ */
+
+public class App5_1 {
+    public static void main(String[] args) {
+        int monthNumber;
+        System.out.println("Введите число от 1 до 12");
+        Scanner scanner = new Scanner(System.in);
+        String month = scanner.nextLine();
+        try {
+            monthNumber = Integer.parseInt(month);
+            if (monthNumber > 0 && monthNumber <= 12) {
+                for (Month month1 : Month.values()) {
+                    if (month1.getNumber() == monthNumber) {
+                        System.out.println(month1.getTitle());
+                        break;
+                    }
+                }
+            } else throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Не допустимое значение!");
+            main(args);
+        }
+    }
+}
+
+enum Month {
+    JANUARY(1, "Январь"),
+    FEBRUARY(2, "Ферраль"),
+    MARCH(3, "Март"),
+    APRIL(4, "Апрель"),
+    MAY(5, "Май"),
+    JUNE(6, "Июнь"),
+    JULY(7, "Июль"),
+    AUGUST(8, "Август"),
+    SEPTEMBER(9, "Сентябрь"),
+    OCTOBER(10, "Октябрь"),
+    NOVENBER(11, "Ноябрь"),
+    DECEMBER(12, "Декабрь");
+
+    private int number;
+    private String title;
+    private Month (int number, String title){
+        this.number = number;
+        this.title = title;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+}
+
