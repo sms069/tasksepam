3ab7e658eb84d471757c0325b5e4e1b140b7325e
Mihail Suhonosov <sms069@bk.ru>
2020-11-19 00:25:16 +0300

    task1 App1 editing

--- task/src/main/java/epam/fundamentals/task1/App1.java
+++ task/src/main/java/epam/fundamentals/task1/App1.java
@@ -7,21 +7,26 @@ package epam.fundamentals.task1;
 
 public class App1 {
     public static void main(String[] args) {
-        Numbers numbers = new Numbers();
-        int min = 0;
-        int max = 0;
-        for (int i = 1; i < numbers.getBinaryNumbers().length; i++) {
-            if (numbers.getBinaryNumbers()[i].length() < numbers.getBinaryNumbers()[min].length()) {
-                min = i;
+        try {
+            Numbers numbers = new Numbers();
+            int min = 0;
+            int max = 0;
+            for (int i = 1; i < numbers.getBinaryNumbers().length; i++) {
+                if (numbers.getBinaryNumbers()[i].length() < numbers.getBinaryNumbers()[min].length()) {
+                    min = i;
+                }
+                if (numbers.getBinaryNumbers()[i].length() > numbers.getBinaryNumbers()[max].length()) {
+                    max = i;
+                }
             }
-            if (numbers.getBinaryNumbers()[i].length() > numbers.getBinaryNumbers()[max].length()) {
-                max = i;
-            }
+            System.out.println("Самое длинное число " + numbers.getNumbers()[max] + " {" + numbers.getBinaryNumbers()[max] + "}\n" +
+                    "его длина = " + numbers.getBinaryNumbers()[max].length());
+            System.out.println("Самое короткое число " + numbers.getNumbers()[min] + " {" + numbers.getBinaryNumbers()[min] + "}\n" +
+                    "его длина = " + numbers.getBinaryNumbers()[min].length());
+        } catch (java.util.InputMismatchException e) {
+            System.err.println("Введено не допустимое значение " + e);
+            System.exit(1);
         }
-        System.out.println("Самое длинное число " + numbers.getNumbers()[max] + " {" + numbers.getBinaryNumbers()[max] + "}\n" +
-                "его длина = " + numbers.getBinaryNumbers()[max].length());
-        System.out.println("Самое короткое число " + numbers.getNumbers()[min] + " {" + numbers.getBinaryNumbers()[min] + "}\n" +
-                "его длина = " + numbers.getBinaryNumbers()[min].length());
     }
 }
 
--- task/src/main/java/epam/fundamentals/task1/Numbers.java
+++ task/src/main/java/epam/fundamentals/task1/Numbers.java
@@ -8,15 +8,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Numbers {
-    private int [] numbers;
-    private String [] binaryNumbers;
+    private int[] numbers;
+    private String[] binaryNumbers;
 
-    public Numbers(){
-        setNumbers();
-        System.out.println(Arrays.toString(getNumbers()));
-        setBeenaryNumbers();
-        System.out.println("Эти числа в двоичной системе исчисления \n" +
-                Arrays.deepToString(getBinaryNumbers()));
+    public Numbers() {
+        NumbersReader numbersReader = new NumbersReader(new Scanner(System.in));
+        this.numbers = numbersReader.readNumbers();
+        this.binaryNumbers = numbersReader.toBinaryNumbers(numbers);
+        printNumbers();
     }
 
     public int[] getNumbers() {
@@ -27,27 +26,9 @@ public class Numbers {
         return this.binaryNumbers;
     }
 
-    public void setBeenaryNumbers() {
-        String binaryNumbers [] = new String[this.numbers.length];
-        for (int i = 0; i < this.numbers.length; i++) {
-            binaryNumbers[i] = Integer.toBinaryString(this.numbers[i]);
-        }
-        this.binaryNumbers = binaryNumbers;
-    }
-
-    public void setNumbers() {
-        Scanner scanner = new Scanner(System.in);
-        int nunber = 0;
-        try {
-            nunber = scanner.nextInt();
-        } catch (java.util.InputMismatchException e) {
-            System.err.println("Не верный ввод " + e);
-            System.exit(0);
-        }
-        int [] numbers = new int[nunber];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int)(Math.random()*100);
-        }
-        this.numbers = numbers;
+    public void printNumbers() {
+        System.out.println(Arrays.toString(this.numbers));
+        System.out.println("Эти числа в двоичной системе исчисления \n" +
+                Arrays.deepToString(this.binaryNumbers));
     }
 }
